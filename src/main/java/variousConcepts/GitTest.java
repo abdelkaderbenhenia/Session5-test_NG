b75e45772a0d0bdc300215377a0e94e167187974
abdelkaderbenhenia <abdelkader.benhenia@gmail.com>
2021-07-31 18:01:03 -0400

    3rd commit

--- src/main/java/variousConcepts/GitTest.java
+++ src/main/java/variousConcepts/GitTest.java
@@ -3,8 +3,13 @@ package variousConcepts;
 public class GitTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		PrintMe();
+	}
+	public static void PrintMe () {
+		
+		
+		System.out.println("git test");
 	}
 
 }
